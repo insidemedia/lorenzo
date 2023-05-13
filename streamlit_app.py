495e0f47b0564bbf4b41f4f2ede24df16645d68d
insidemedia <insidemedia@gmail.com>
2023-05-13 00:40:57 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,26 +6,31 @@ from hugchat import hugchat
 
 st.set_page_config(page_title="HugChat - An LLM-powered Streamlit app")
 
+# Sidebar contents
 with st.sidebar:
     st.title('🤗💬 HugChat App')
     st.markdown('''
     ## About
     This app is an LLM-powered chatbot built using:
-    - [Streamlit](<https://streamlit.io/>)
-    - [HugChat](<https://github.com/Soulter/hugging-chat-api>)
-    - [OpenAssistant/oasst-sft-6-llama-30b-xor](<https://huggingface.co/OpenAssistant/oasst-sft-6-llama-30b-xor>) LLM model
+    - [Streamlit](https://streamlit.io/)
+    - [HugChat](https://github.com/Soulter/hugging-chat-api)
+    - [OpenAssistant/oasst-sft-6-llama-30b-xor](https://huggingface.co/OpenAssistant/oasst-sft-6-llama-30b-xor) LLM model
     
     💡 Note: No API key required!
     ''')
     add_vertical_space(5)
-    
-    if 'generated' not in st.session_state:
-    st.session_state['generated'] = ["I'm HugChat, How may I help you?"]
+    st.write('Made with ❤️ by [Data Professor](https://youtube.com/dataprofessor)')
 
+# Generate empty lists for generated and past.
+## generated stores AI generated responses
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = ["I'm HugChat, How may I help you?"]
+## past stores User's questions
 if 'past' not in st.session_state:
     st.session_state['past'] = ['Hi!']
-    
-    input_container = st.container()
+
+# Layout of input/response containers
+input_container = st.container()
 colored_header(label='', description='', color_name='blue-30')
 response_container = st.container()
 
@@ -34,19 +39,17 @@ response_container = st.container()
 def get_text():
     input_text = st.text_input("You: ", "", key="input")
     return input_text
-
 ## Applying the user input box
 with input_container:
     user_input = get_text()
-    
-    # Response output
+
+# Response output
 ## Function for taking user prompt as input followed by producing AI generated responses
 def generate_response(prompt):
     chatbot = hugchat.ChatBot()
     response = chatbot.chat(prompt)
     return response
-  
-  
+
 ## Conditional display of AI generated responses as a function of user provided prompts
 with response_container:
     if user_input:
@@ -57,6 +60,6 @@ with response_container:
     if st.session_state['generated']:
         for i in range(len(st.session_state['generated'])):
             message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
-            message(st.session_state['generated'][i], key=str(i))
+            message(st.session_state["generated"][i], key=str(i))
             
     
